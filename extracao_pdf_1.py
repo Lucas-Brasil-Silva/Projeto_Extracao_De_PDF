afe267c4176fc9fa941437c5a107c6609f376123
Lucas Brasil Silva <lucasbrasil396@gmail.com>
2023-10-29 14:27:46 -0300

    added documentation

--- extracao_pdf_1.py
+++ extracao_pdf_1.py
@@ -1,31 +1,47 @@
+# Importando as bibliotecas necessárias
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer
 from openpyxl import Workbook
 import re
 
-texto = []
-for page_layout in extract_pages('pdfs\pdf-1.pdf'):
-    for element in page_layout:
-        if isinstance(element,LTTextContainer):
-            dados = element.get_text().split('\n')
-            organizacao = dados[0] if len(dados) < 3 and dados[0] != ' ' else organizacao
-            if len(dados) > 2:
-                instituicao,cargo,responsavel,telefone,email,endereco = None,None,None,None,None,None
-                for dado in dados[:-1]:
-                    instituicao = dado if not re.findall(r':',dado) else instituicao
-                    cargo = dado.split(':')[0] if not dado == instituicao and not re.findall(r'Telefone:',dado) and not re.findall(r'Email',dado) and not re.findall(r'Endereço:',dado) else cargo
-                    responsavel = dado.split(':')[1] if not dado == instituicao and not re.findall(r'Telefone:',dado) and not re.findall(r'Email',dado) and not re.findall(r'Endereço:',dado) else responsavel
-                    telefone = dado.replace('Telefone: ','').replace('Celular: ','') if re.findall(r'Telefone: ',dado) else telefone
-                    email = dado.replace('Email institucional: ','').replace('Email: ','') if re.findall(r'Email',dado) else email
-                    endereco = dado.replace('Endereço: ','') if re.findall(r'Endereço: ',dado) else endereco
-                texto.append([organizacao,instituicao,cargo,responsavel,telefone,email,endereco])
+def main(planilha):
+    """
+    Extrai informações de um PDF e as armazena em um arquivo Excel.
 
-workbook = Workbook()
-workbook['Sheet'].title = 'Dados PDF'
-sheet = workbook['Dados PDF']
-sheet.append(['Organizacao','Instituicao','Cargo','Resposavel','Telefone','Email','Endereco'])
+    :param planilha: Nome do arquivo Excel de saída.
+    :type planilha: str
+    """
 
-for linha in texto:
-    sheet.append(linha)
+    texto = []
+    for page_layout in extract_pages('pdfs\pdf-1.pdf'):
+        for element in page_layout:
+            if isinstance(element,LTTextContainer):
+                dados = element.get_text().split('\n')
+                # Organização é extraída se não houver cabeçalho de página
+                organizacao = dados[0] if len(dados) < 3 and dados[0] != ' ' else organizacao
+                # Processamento de informações da instituição
+                if len(dados) > 2:
+                    instituicao,cargo,responsavel,telefone,email,endereco = None,None,None,None,None,None
+                    for dado in dados[:-1]:
+                        instituicao = dado if not re.findall(r':',dado) else instituicao
+                        cargo = dado.split(':')[0] if not dado == instituicao and not re.findall(r'Telefone:',dado) and not re.findall(r'Email',dado) and not re.findall(r'Endereço:',dado) else cargo
+                        responsavel = dado.split(':')[1] if not dado == instituicao and not re.findall(r'Telefone:',dado) and not re.findall(r'Email',dado) and not re.findall(r'Endereço:',dado) else responsavel
+                        telefone = dado.replace('Telefone: ','').replace('Celular: ','') if re.findall(r'Telefone: ',dado) else telefone
+                        email = dado.replace('Email institucional: ','').replace('Email: ','') if re.findall(r'Email',dado) else email
+                        endereco = dado.replace('Endereço: ','') if re.findall(r'Endereço: ',dado) else endereco
+                    texto.append([organizacao,instituicao,cargo,responsavel,telefone,email,endereco])
 
-workbook.save('tabelas\dados-PDF-1.xlsx')
+    # Criação de um arquivo Excel e uma planilha para armazenar os dados
+    workbook = Workbook()
+    workbook['Sheet'].title = 'Dados PDF'
+    sheet = workbook['Dados PDF']
+    sheet.append(['Organizacao','Instituicao','Cargo','Resposavel','Telefone','Email','Endereco'])
+
+    # Preenchimento da planilha com os dados extraídos
+    for linha in texto:
+        sheet.append(linha)
+    # Salvamento do arquivo Excel com o nome fornecido
+    workbook.save(f'tabelas\{planilha}.xlsx')
+
+if __name__ == '__main__':
+    main('dados-PDF-1')
--- extracao_pdf_2.py
+++ extracao_pdf_2.py
@@ -1,31 +1,50 @@
+# Importando as bibliotecas necessárias
 from pdfminer.high_level import extract_text, extract_pages
 from pdfminer.layout import LTTextContainer
 from openpyxl import Workbook
 import re
 
-matrix_dados = []
-for page_layout in extract_pages('pdfs\pdf-2.pdf'):
-    for element in page_layout:
-        if isinstance(element,LTTextContainer):
-            texto = re.sub(r'(?<=\S)\s(?=\S)','',element.get_text())
-            texto_ = re.sub(r'\s+',' ',texto)
-            matrix_dados.append(texto_.split('\n'))
+def main(planilha):
+    """
+    Extrai informações de um PDF e as armazena em um arquivo Excel.
 
-matrix_filtrada = [dado for dado in matrix_dados if not dado[0] == ' '][2:]
+    :param planilha: Nome do arquivo Excel de saída.
+    :type planilha: str
+    """
 
-instituicao = [[id,dado[0]] for id,dado in enumerate(matrix_filtrada) if not re.findall(r'(TELEFONE:)',dado[0]) and not re.findall(r'(http)',dado[0]) and not re.findall(r'(E-mail:)',dado[0]) and not re.findall(r'(Saiba)',dado[0])]
+    matrix_dados = []
+    for page_layout in extract_pages('pdfs\pdf-2.pdf'):
+        for element in page_layout:
+            if isinstance(element,LTTextContainer):
+                texto = re.sub(r'(?<=\S)\s(?=\S)','',element.get_text())
+                texto_ = re.sub(r'\s+',' ',texto)
+                matrix_dados.append(texto_.split('\n'))
 
-dados = [matrix_filtrada[linha[0]+1:instituicao[id+1][0]] if id+1 < len(instituicao) else matrix_filtrada[linha[0]+1:] for id,linha in enumerate(instituicao)]
+    # Filtra os dados da matriz
+    matrix_filtrada = [dado for dado in matrix_dados if not dado[0] == ' '][2:]
 
-workbook = Workbook()
-workbook['Sheet'].title = 'Dados-PDF'
-sheet = workbook['Dados-PDF']
-sheet.append(['Instituição','Telefone','E-mail','Site'])
+    # Identifica as instituições e suas informações
+    instituicao = [[id,dado[0]] for id,dado in enumerate(matrix_filtrada) if not re.findall(r'(TELEFONE:)',dado[0]) and not re.findall(r'(http)',dado[0]) and not re.findall(r'(E-mail:)',dado[0]) and not re.findall(r'(Saiba)',dado[0])]
 
-for id,linha in enumerate(dados):
-    telefone = ''.join(item[0].replace('TELEFONE: ','') for item in linha if re.findall(r'(TELEFONE:)', item[0]))
-    email = ''.join(re.sub(r'\s+', '', item[0].replace('E-mail: ','')) for item in linha if re.findall(r'(E-mail)', item[0]))
-    link = ''.join(re.sub(r'\s+', '', item[0].replace('Saiba mais:','').replace('FALE CONOSCO ON-LINE:','').replace('Saibamais:','')) for item in linha if re.findall(r'(http)', item[0]))
-    sheet.append([instituicao[id][1],telefone,email,link])
+    # Extrai os dados das instituições
+    dados = [matrix_filtrada[linha[0]+1:instituicao[id+1][0]] if id+1 < len(instituicao) else matrix_filtrada[linha[0]+1:] for id,linha in enumerate(instituicao)]
 
-workbook.save('tabelas\dados-PDF-2.xlsx')
+    # Cria um arquivo Excel e uma planilha para armazenar os dados
+    workbook = Workbook()
+    workbook['Sheet'].title = 'Dados-PDF'
+    sheet = workbook['Dados-PDF']
+    sheet.append(['Instituição','Telefone','E-mail','Site'])
+
+    # Preenche a planilha com os dados
+    for id,linha in enumerate(dados):
+        telefone = ''.join(item[0].replace('TELEFONE: ','') for item in linha if re.findall(r'(TELEFONE:)', item[0]))
+        email = ''.join(re.sub(r'\s+', '', item[0].replace('E-mail: ','')) for item in linha if re.findall(r'(E-mail)', item[0]))
+        link = ''.join(re.sub(r'\s+', '', item[0].replace('Saiba mais:','').replace('FALE CONOSCO ON-LINE:','').replace('Saibamais:','')) for item in linha if re.findall(r'(http)', item[0]))
+        sheet.append([instituicao[id][1],telefone,email,link])
+
+    # Salva o arquivo Excel
+    workbook.save(f'tabelas\{planilha}.xlsx')
+
+# Chama a função principal com o nome do arquivo de saída desejado
+if __name__ == '__main__':
+    main('dados-PDF-2')
